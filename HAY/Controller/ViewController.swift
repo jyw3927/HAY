79d45bdf8bafbea427e0987b34a0a80d284bde40
jyw3927 <nagne3927@gmail.com>
2022-06-19 16:57:26 +0900

    Extension(BackgroundView & Color) added

--- HAY/Controller/ViewController.swift
+++ HAY/Controller/ViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var bgView: UIView!
+    
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        bgView.setGradient(color1: .primary ?? .white, color2: .subPrimary ?? .black)
     }
 
 
---
+++ HAY/Extensions/Color.swift
@@ -0,0 +1,42 @@
+//
+//  Color.swift
+//  HAY
+//
+//  Created by 전영우 on 2022/06/19.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    class var primary: UIColor? { return UIColor(named: "Primary") }
+    
+    class var subPrimary: UIColor? { return UIColor(named: "SubPrimary") }
+    
+    class var secondary: UIColor? { return UIColor(named: "Secondary") }
+    
+    class var onPrimary: UIColor? { return UIColor(named: "OnPrimary") }
+    
+    class var disabled: UIColor? { return UIColor(named: "Disabled") }
+    
+    class var onSecondary: UIColor? { return UIColor(named: "OnSecondary") }
+    
+    class var onBackground: UIColor? { return UIColor(named: "OnBackground") }
+    
+    class var detail: UIColor? { return UIColor(named: "Detail") }
+    
+    class var categoryBar: UIColor? { return UIColor(named: "CategoryBar") }
+    
+    class var onCategoryBar: UIColor? { return UIColor(named: "OnCategoryBar") }
+    
+    class var cardBar: UIColor? { return UIColor(named: "CardBar") }
+    
+    class var subBackground: UIColor? { return UIColor(named: "SubBackground") }
+    
+    class var onSubBackground: UIColor? { return UIColor(named: "OnSubBackground") }
+    
+    class var disabledButton: UIColor? { return UIColor(named: "DisabledButton") }
+    
+    class var error: UIColor? { return UIColor(named: "Error") }
+    
+}
