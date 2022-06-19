43c08fee73e92f5c876bfeeb21737731f5f93667
jyw3927 <nagne3927@gmail.com>
2022-06-19 16:04:55 +0900

    Project launched

---
+++ HAY/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HAY
+//
+//  Created by 전영우 on 2022/06/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
